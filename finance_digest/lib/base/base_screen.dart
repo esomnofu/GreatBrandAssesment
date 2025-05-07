2fe5a324c3bf741da39158766b4677b39629d0bb
Christian Esomnofu <mresomnofu@gmail.com>
2025-05-07 11:40:00 +0100

    ref: added project base and loader

---
+++ finance_digest/lib/base/base_screen.dart
@@ -0,0 +1,39 @@
+import 'package:finance_digest/base/base_vm.dart';
+import 'package:finance_digest/widgets/loader.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class BaseView<T extends BaseViewModel> extends StatelessWidget {
+  final T Function(BuildContext) vmBuilder;
+  final Widget Function(BuildContext, T) builder;
+
+  const BaseView({Key? key, required this.vmBuilder, required this.builder})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<T>.value(
+      value: vmBuilder(context),
+      child: Consumer<T>(
+        builder: _buildScreenContent,
+      ),
+    );
+  }
+
+  Widget _buildScreenContent(BuildContext context, T viewModel, Widget? child) {
+    if (!viewModel.isInitialized && viewModel.isLoading == true) {
+      return Container(
+          color: Colors.blue, child: const Center(child: Loader()));
+    } else {
+      return Stack(
+        children: [
+          builder(context, viewModel),
+          Visibility(
+            visible: viewModel.isLoading,
+            child: const Center(child: Loader()),
+          )
+        ],
+      );
+    }
+  }
+}
---
+++ finance_digest/lib/widgets/loader.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Loader extends StatelessWidget {
+  const Loader({
+    super.key,
+    this.height = 45,
+    this.width = 45,
+    this.strokeWidth = 8,
+  });
+  final double height;
+  final double width;
+  final double strokeWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(
+          color: Colors.blueAccent.withValues(alpha: 0.4),
+        ),
+        SizedBox(
+          width: width,
+          height: height,
+          child: CircularProgressIndicator(
+            strokeWidth: strokeWidth,
+            backgroundColor: Colors.blue,
+            valueColor: AlwaysStoppedAnimation<Color>(
+              Colors.grey.withValues(alpha: 0.7),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
